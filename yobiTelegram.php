355cf996aa1d7385a8f53d2541eb90b36db1d69d
5hev <s-h-e-v@mail.ru>
2017-07-13 14:17:15 +0300

    Update yobiTelegram.php

--- yobiTelegram.php
+++ yobiTelegram.php
@@ -115,7 +115,7 @@ function processMessage($message) {
     apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
   }
 }
-define('WEBHOOK_URL', 'https://raw.githubusercontent.com/5hev/yobitTelegram/edit/master/yobiTelegram.php');
+define('WEBHOOK_URL', 'https://rawgit.com/5hev/yobitTelegram/master/yobiTelegram.php');
 if (php_sapi_name() == 'cli') {
   // if run from console, set or delete webhook
   apiRequest('setWebhook', array('url' => isset($argv[1]) && $argv[1] == 'delete' ? '' : WEBHOOK_URL));
